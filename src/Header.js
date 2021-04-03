eb6bd02c86d66807969981ef67c03e142e71a23b
Nakula <nakulamarvellio@gmail.com>
2021-04-03 22:09:29 +0700

    ADD: Header Options & styling

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,9 @@ import SearchIcon from '@material-ui/icons/Search';
 import HeaderOption from './HeaderOption';
 import HomeIcon from '@material-ui/icons/Home';
 import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount';
-
+import BusinessCenterIcon from '@material-ui/icons/BusinessCenter';
+import ChatIcon from '@material-ui/icons/Chat';
+import NotificationsIcon from '@material-ui/icons/Notifications';
 
 function Header() {
     return (
@@ -20,6 +22,12 @@ function Header() {
             <div className="header__right">
                 <HeaderOption Icon={HomeIcon}title="Home"/>
                 <HeaderOption Icon={SupervisorAccountIcon} title="My Network"/>
+                <HeaderOption Icon={BusinessCenterIcon} title="Jobs"/>
+                <HeaderOption Icon={ChatIcon} title="Messaging"/>
+                <HeaderOption Icon={NotificationsIcon} title="Notifications"/>
+
+                <HeaderOption 
+                    avatar="https://compassionate-leakey-e9b16b.netlify.app/images/IG_Sonny.jpeg" title="me" />
             </div>
         </div>
     )
