1c81d8e19d4d7699bbcaa6a6a2cec591afd33c6f
Nakula <nakulamarvellio@gmail.com>
2021-04-21 02:28:10 +0700

    CHGN: Debuging the Login and Redux files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import React, {useSelector} from 'react';
+import React from 'react';
+import { useSelector } from 'react-redux';
 import './App.css';
 import Header from './Header';
 import Sidebar from './Sidebar';
 import Feed from './Feed';
-import { selectUser } from './features/userSlice';
 import Login from './Login';
+import { selectUser } from './features/userSlice';
+
 
 function App() {
   const user = useSelector(selectUser);
-
-
 
   return (
     <div className="app">
       <Header />
 
-      {!user ? (
-      <Login /> 
-      ) :(
+      {!user ? (<Login /> ): (
         <div className="app__body">
         <Sidebar />
         <Feed />
@@ -27,5 +25,6 @@ function App() {
     </div>
   );
 }
+
 
 export default App;
--- src/Login.js
+++ src/Login.js
@@ -4,9 +4,10 @@ import './Login.css';
 function Login() {
     return (
         <div className="login">
+            <h1>Your are not logged in</h1>
             
         </div>
-    )
+    );
 }
 
-export default Login
+export default Login;
